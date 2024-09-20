9d253c71748ca8ab5cfce6276aa2f41990f184a5
jm.maya <jm.maya@106-20.info2>
2024-09-20 14:18:26 +0200

    Ejercicio cinco actualizado y terminado

--- src/exercise5/Exercise5.java
+++ src/exercise5/Exercise5.java
@@ -4,8 +4,7 @@ public class Exercise5 {
 
 	public static void main(String[] args) {
 		System.out.println("Name: Jose Manuel");
-		System.out.println("Address: Calle Quevedo Nº 11º\n");
-		// Puedo no poner el \n y optar por System.out.println() en esta línea;
+		System.out.println("Address: Calle Quevedo Nº 11º");
 		System.out.println("Telephone Number: 629263572");
 
 	}
